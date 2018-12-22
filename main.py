b282d4e8b6b7cdec02c02e246ad50114e4812d21
Lcarused <donghao19950925@gmail.com>
2018-12-22 14:13:57 +0800

    主程序

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 # -*- coding:utf-8 -*-
 
+import friend_analysis
+import baidu_api
+
+''' 主程序 '''
+
+def friend_parser():
+    friend_analysis.start()
+
+
+if __name__ == "__main__":
+    # 微信好友分析
+    friend_parser()
