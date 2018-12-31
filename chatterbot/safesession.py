f56b21ee61cb0722b4cb6137b9f18767a2be4ec7
Lcarused <donghao19950925@gmail.com>
2018-12-31 21:58:49 +0800

    全部代码注释掉，迁移到botbase文件

--- chatterbot/safesession.py
+++ chatterbot/safesession.py
@@ -1,21 +1,21 @@
 # -*- coding:utf-8 -*-
 
-import traceback
-
-import requests
-
-
-class SafeSession(requests.Session):
-    def request(self, method, url, params=None, data=None, headers=None, cookies=None, files=None, auth=None, timeout=None, allow_redirects=True, proxies=None, hooks=None, stream=None, verify=None, cert=None, json=None):
-        for i in range(3):
-            try:
-                return super(SafeSession, self).request(method, url, params, data, headers, cookies, files, auth, timeout, allow_redirects, proxies, hooks, stream, verify, cert, json)
-            except Exception as e:
-                print (e.message, traceback.format_exc())
-                continue
-
-        # 重试3次以后再加一次，抛出异常
-        try:
-            return super(SafeSession, self).request(method, url, params, data, headers, cookies, files, auth, timeout, allow_redirects, proxies, hooks, stream, verify, cert, json)
-        except Exception as e:
-            raise e
+# import traceback
+#
+# import requests
+#
+#
+# class SafeSession(requests.Session):
+#     def request(self, method, url, params=None, data=None, headers=None, cookies=None, files=None, auth=None, timeout=None, allow_redirects=True, proxies=None, hooks=None, stream=None, verify=None, cert=None, json=None):
+#         for i in range(3):
+#             try:
+#                 return super(SafeSession, self).request(method, url, params, data, headers, cookies, files, auth, timeout, allow_redirects, proxies, hooks, stream, verify, cert, json)
+#             except Exception as e:
+#                 print (e.message, traceback.format_exc())
+#                 continue
+#
+#         # 重试3次以后再加一次，抛出异常
+#         try:
+#             return super(SafeSession, self).request(method, url, params, data, headers, cookies, files, auth, timeout, allow_redirects, proxies, hooks, stream, verify, cert, json)
+#         except Exception as e:
+#             raise e
