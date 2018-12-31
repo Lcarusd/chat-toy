bc7ce512c6d9b0eeabbff0ca7aaedf213263c1f6
Lcarused <donghao19950925@gmail.com>
2018-12-31 21:59:00 +0800

    全部代码注释掉，迁移到botbase文件

--- chatterbot/tools.py
+++ chatterbot/tools.py
@@ -1,16 +1,15 @@
 # coding: utf-8
 
-
-class Tools(object):
-
-    @staticmethod
-    def to_unicode(string, encoding='utf-8'):
-        """
-        将字符串转换为Unicode
-        """
-        if isinstance(string, str):
-            return string.decode(encoding)
-        elif isinstance(string, unicode):
-            return string
-        else:
-            raise Exception('Unknown Type')
+# class Tools(object):
+#
+#     @staticmethod
+#     def to_unicode(string, encoding='utf-8'):
+#         """
+#         将字符串转换为Unicode
+#         """
+#         if isinstance(string, str):
+#             return string.decode(encoding)
+#         elif isinstance(string, unicode):
+#             return string
+#         else:
+#             raise Exception('Unknown Type')
