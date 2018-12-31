7e8887fd05b79505d28e4ce56130fd95717517ff
Lcarused <donghao19950925@gmail.com>
2018-12-31 21:59:15 +0800

    全部代码注释掉，迁移到botbase文件

--- chatterbot/chat_bot.py
+++ chatterbot/chat_bot.py
@@ -1,25 +1,25 @@
 # coding: utf-8
 
-from botbase import ChatBotBase
-import requests
-bot_api = "http://127.0.0.1:8000/get_response"
-
-
-class ChatBot(ChatBotBase):
-    def handle_msg_all(self, msg):
-        if msg['msg_type_id'] == 4 and msg['content']['type'] == 0:
-            user_input = msg["content"]["data"]
-            payload = {"user_input": user_input}
-            response = requests.get(bot_api, params=payload).json()["response"]
-            self.send_msg_by_uid(response, msg['user']['id'])
-
-
-def main():
-    bot = ChatBot()
-    bot.DEBUG = True
-    bot.conf['qr'] = 'png'
-    bot.run()
-
-
-if __name__ == '__main__':
-    main()
+# from botbase import ChatBotBase
+# import requests
+# bot_api = "http://127.0.0.1:8000/get_response"
+#
+#
+# class ChatBot(ChatBotBase):
+#     def handle_msg_all(self, msg):
+#         if msg['msg_type_id'] == 4 and msg['content']['type'] == 0:
+#             user_input = msg["content"]["data"]
+#             payload = {"user_input": user_input}
+#             response = requests.get(bot_api, params=payload).json()["response"]
+#             self.send_msg_by_uid(response, msg['user']['id'])
+#
+#
+# def main():
+#     bot = ChatBot()
+#     bot.DEBUG = True
+#     bot.conf['qr'] = 'png'
+#     bot.run()
+#
+#
+# if __name__ == '__main__':
+#     main()
