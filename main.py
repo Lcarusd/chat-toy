d6cd01195ade7c70a35794f988beb0bd58466011
Lcarused <donghao19950925@gmail.com>
2019-01-01 21:25:50 +0800

    主程序，入口文件(未完成)

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 # -*- coding:utf-8 -*-
 
+import itchat
+
 import friend_analysis
 import baidu_api
+from wechat_media_download import test
 
-''' 主程序 '''
+# def itchat_config():
+# itchat_login = itchat.auto_login(hotReload=True)
+# itchat_run =  itchat.run()
+# itchat_run.test()
+# itchat_run.itchat.send(u'测试消息发送', 'filehelper')
+    # return itchat_run
 
-def friend_parser():
-    friend_analysis.start()
-
-
-if __name__ == "__main__":
-    # 微信好友分析
-    friend_parser()
+# ''' 主程序 '''
+#
+# def friend_parser():
+#     friend_analysis.start()
+#
+#
+# if __name__ == "__main__":
+#     # 微信好友分析
+#     friend_parser()
